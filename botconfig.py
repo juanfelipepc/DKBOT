854466e527e85235ba2b92fc470d87d350a343a4
Juan Perilla <49820615+juanfelipepc@users.noreply.github.com>
2021-01-15 20:39:38 -0500

    Add bot owner variable to botconfig

--- botconfig.py
+++ botconfig.py
@@ -1,17 +1,20 @@
-#irc server address
+# irc server address
 sv_address = ''
 
-#irc server port
+# irc server port
 sv_port = 0
 
-#irc user password
+# irc user password
 us_pass = ''
 
-#irc user nickname
+# irc user nickname
 us_nick = ''
 
-#control token for the telegram API bot
+# control token for the telegram API bot
 telegram_token = ''
 
-#group id for the group or user the bot is to send messages to
+# group id for the group or user the bot is to send messages to
 telegram_group_id = ''
+
+# bot owner
+bot_owner = ''
