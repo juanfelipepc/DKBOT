04780ef66de7b2e6e5d72fdfa35a6754ffd019ca
Juan Perilla <49820615+juanfelipepc@users.noreply.github.com>
2021-01-16 09:19:35 -0500

    Add listen loop

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+import re
+
 import irc
 import telegram
-from botconfig import sv_address, sv_port, us_pass, us_nick
+from botconfig import sv_address, sv_port, us_pass, us_nick, irc_channel
 
+message_data = ''
+GENERAL_MSG_REGEX = re.compile(r':(.*)!.*@.* .* (.*) :(.*)')
 
 irc.connect(sv_address, sv_port, us_pass, us_nick)
 
 while 1:
-    irc.listen()
+    message_data = irc.listen(message_data)
+    gen_msg = GENERAL_MSG_REGEX.search(str(message_data))
+
+    if gen_msg:
+        print("MAIN:")
+        print(gen_msg.groups())
+        msg_source = gen_msg.group(2)
+        msg_text = gen_msg.group(3)
+
+        if msg_source == irc_channel:
+            print(msg_text)
 
 # telegram.get_updates()
 # telegram.send_message('', '')
