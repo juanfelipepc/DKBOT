0b596f246aff1d67429a0ee0b5443e81facf82cf
Juan Perilla <49820615+juanfelipepc@users.noreply.github.com>
2021-01-15 20:31:32 -0500

    Add telegram module

---
+++ telegram.py
@@ -0,0 +1,32 @@
+import requests
+
+from botconfig import telegram_token
+
+API_URL = 'https://api.telegram.org'
+ROOT_URL = f'{API_URL}/bot{telegram_token}/'
+
+
+def get_updates():
+    UPDATES_URL = ROOT_URL + 'getUpdates'
+    update_params = {'offset': '-1', 'limit': '1'}
+        
+    try:
+        response = requests.get(UPDATES_URL, params=update_params)
+        data = response.json()
+        print(data)
+    except:
+        raise ValueError("Failed to get updates")
+    
+    return data
+
+
+def send_message(group_id, message):
+    SEND_URL = ROOT_URL + 'sendMessage'
+    msg_params = {'chat_id': group_id, 'text': message}
+
+    try:
+        response = requests.post(SEND_URL, params=msg_params)
+        data = response.json()
+        print(data)
+    except:
+        raise ValueError("Failed to send message")
