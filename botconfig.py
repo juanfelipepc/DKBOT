8ed6b5fa75bbf5ec8db9a3c3223530fa86477277
Juan Perilla <49820615+juanfelipepc@users.noreply.github.com>
2021-01-15 20:30:32 -0500

    Add config file for bot variables

---
+++ botconfig.py
@@ -0,0 +1,17 @@
+#irc server address
+sv_address = ''
+
+#irc server port
+sv_port = 0
+
+#irc user password
+us_pass = ''
+
+#irc user nickname
+us_nick = ''
+
+#control token for the telegram API bot
+telegram_token = ''
+
+#group id for the group or user the bot is to send messages to
+telegram_group_id = ''
