3b8b9ade99365c209751a771c4fb6e81313a52d4
Juan Perilla <49820615+juanfelipepc@users.noreply.github.com>
2021-01-19 17:19:20 -0500

    Add IRC -> Telegram one way communication

--- main.py
+++ main.py
@@ -2,25 +2,27 @@ import re
 
 import irc
 import telegram
-from botconfig import sv_address, sv_port, us_pass, us_nick, irc_channel
+from botconfig import sv_address, sv_port, us_pass, us_nick, irc_channel, telegram_group_id
 
 message_data = ''
+prev_telegram_msg = ''
 GENERAL_MSG_REGEX = re.compile(r':(.*)!.*@.* .* (.*) :(.*)')
 
 irc.connect(sv_address, sv_port, us_pass, us_nick)
 
 while 1:
     message_data = irc.listen(message_data)
+    telegram_msg = telegram.get_updates()
     gen_msg = GENERAL_MSG_REGEX.search(str(message_data))
 
     if gen_msg:
-        print("MAIN:")
-        print(gen_msg.groups())
-        msg_source = gen_msg.group(2)
-        msg_text = gen_msg.group(3)
+        msg_user, msg_source, msg_text = gen_msg.group(1), gen_msg.group(2), gen_msg.group(3)
 
-        if msg_source == irc_channel:
+        if msg_source == (f'#{irc_channel}'):
             print(msg_text)
+            telegram.send_message(telegram_group_id, f'[{msg_user}]: {msg_text}')
 
-# telegram.get_updates()
-# telegram.send_message('', '')
+    # Temporary telegram message checking for testing purposes
+    if telegram_msg and (prev_telegram_msg != telegram_msg):
+        prev_telegram_msg = telegram_msg
+        print(telegram_msg)
